059e0a03be197f5c3d12a16b6230ba0b1666d839
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-01-29 17:50:38 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -90,14 +90,48 @@ def num_factors(n):
     return div
 
 """
+A python implementation of the Rabin-Miller algorithm.
+"""
+
+def is_prime_prob(n, k=10):
+    if n == 2:
+        return True
+    if not n & 1:
+        return False
+
+    def check(a, s, d, n):
+        x = pow(a, d, n)
+        if x == 1:
+            return True
+        for i in range(s - 1):
+            if x == n - 1:
+                return True
+            x = pow(x, 2, n)
+        return x == n - 1
+
+    s = 0
+    d = n - 1
+
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+
+    for i in range(k):
+        a = randrange(2, n - 1)
+        if not check(a, s, d, n):
+            return False
+    return True
+
+"""
 A python implementation of the Sieve of Eratosthenes.
-
-Include the function by adding:
-import sieve
-
 """
 
 def prime_sieve(limit):
+    
+    # The following line fails with arbitrarily large integers
+    # Workaround is
+    # a = [True for _ in range(limit)]
+  
     a = [True] * limit                         
     a[0] = a[1] = False
 
@@ -110,6 +144,7 @@ def prime_sieve(limit):
     return a
 
 if __name__ == '__main__':
+    # Just a placeholder test
     sieved = prime_sieve(1000000)
     print("Sieve successful.")
 
