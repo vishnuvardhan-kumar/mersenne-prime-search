039cc77da7186b46391eb122f671943d89926a90
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-01-29 17:47:54 +0530

    Moved function to library

--- mersenne.py
+++ mersenne.py
@@ -1,35 +1,6 @@
 from random import randrange
+from prime import is_prime_prob
 import time
-
-def is_prime(n, k=10):
-    if n == 2:
-        return True
-    if not n & 1:
-        return False
-
-    def check(a, s, d, n):
-        x = pow(a, d, n)
-        if x == 1:
-            return True
-        for i in range(s - 1):
-            if x == n - 1:
-                return True
-            x = pow(x, 2, n)
-        return x == n - 1
-
-    s = 0
-    d = n - 1
-
-    while d % 2 == 0:
-        d >>= 1
-        s += 1
-
-    for i in range(k):
-        a = randrange(2, n - 1)
-        if not check(a, s, d, n):
-            return False
-    return True
-
 
 base = 2
 exponent = 2000
