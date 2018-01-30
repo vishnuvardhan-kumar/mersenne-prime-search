f87250fe21b800b2600c445275d56aa3b08183a5
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-01-30 16:40:10 +0000

    Implemented check function

--- rabinmiller.cpp
+++ rabinmiller.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <math>
 
 bool is_prime_prob(int, int);
 bool check(int,int,int,int);
@@ -10,11 +11,25 @@ int main()
 }
 
 
+bool check(int a, int s, int d, int n)
+{
+	x = pow(a, d, n);
+	if(x == 1)
+		return true;
+	for(int i = 0; i < s - 1; i++){
+		if(x == n - 1)
+			return true;
+		x = pow(x, 2, n)
+	}
+	return x == n - 1
+}
+
 bool is_prime_prob(int n, int k)
 {
 	if(n==2)
 		return true;
 	if(n%2!=0)
 		return false;
+
 
 }
