e56d8785bdb97c2b7ad234f92d475e7c9fca1338
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-02 22:36:31 +0530

    Create rabinmiller_nmod.py

---
+++ rabinmiller_nmod.py
@@ -0,0 +1,22 @@
+import random
+ 
+_mrpt_num_trials = 5 
+ 
+def is_probable_prime(n):
+    assert n >= 2
+    # special case 2
+    if n == 2:
+        return True
+    # ensure n is odd
+    if n % 2 == 0:
+        return False
+    s = 0
+    d = n-1
+    while True:
+        quotient, remainder = divmod(d, 2)
+        if remainder == 1:
+            break
+        s += 1
+        d = quotient
+    assert(2**s * d == n-1)
+ 
