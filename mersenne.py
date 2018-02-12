772e1f78e21d96ea75776428d36ab0458af0c013
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-12 15:10:17 +0000

    Added main decorator

--- mersenne.py
+++ mersenne.py
@@ -7,24 +7,25 @@ starttime = time.time()
 count = 0
 cache = []
 
-try:
+if __name__ == "__main__":
+    try:
 
-    # Base cases
-    assert isinstance(count, int)
-    assert isinstance(cache, list)
-    assert isinstance(base, int)
-    assert isinstance(exponent, int)
+        # Base cases
+        assert isinstance(count, int)
+        assert isinstance(cache, list)
+        assert isinstance(base, int)
+        assert isinstance(exponent, int)
 
-    while True:
-        cur = time.time()
-        print(f"Checking {base}^{exponent}... ", end='')
-        if is_prime_prob(base**exponent-1):
-            print(f"M({exponent}) is probably prime")
-            break
-        exponent+=2
-        count+=1
-        end = time.time() - cur
-        print(f"{end:.2f} seconds")
-except KeyboardInterrupt:
-    cache.append(count)
-    print("Found {} probable primes in {} seconds".format(count, time.time()-starttime))    
+        while True:
+            cur = time.time()
+            print(f"Checking {base}^{exponent}... ", end='')
+            if is_prime_prob(base**exponent-1):
+                print(f"M({exponent}) is probably prime")
+                break
+            exponent+=2
+            count+=1
+            end = time.time() - cur
+            print(f"{end:.2f} seconds")
+    except KeyboardInterrupt:
+        cache.append(count)
+        print("Found {} probable primes in {} seconds".format(count, time.time()-starttime))    
