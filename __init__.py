9a31cc9a7898804168ed2837547e5a407961a145
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-18 12:45:34 +0000

    Updated __init__

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ if __name__ == '__main__':
 	try:
 		from prime import *
 	except ImportError:
-		print "prime.py not found."
+		print "prime.py not found. Run 'git clone' to get the repositary from the server."
